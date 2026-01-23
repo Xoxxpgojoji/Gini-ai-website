bd755755f1bf3dbaa0cadc0e3d8e9447aa9569fd
Anmol srivastava <155183109+Xoxxpgojoji@users.noreply.github.com>
2026-01-23 19:53:21 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,25 @@
 /* slider */
+const slider = document.querySelector("[data-slider='model1']");
+if (slider) {
+  const slides = slider.querySelector(".slides");
+  const images = slides.querySelectorAll("img");
+  const prev = slider.querySelector(".prev");
+  const next = slider.querySelector(".next");
+
+  let index = 0;
+  const width = 320;
+
+  function updateSlide() {
+    slides.style.transform = `translateX(-${index * width}px)`;
+  }
+
+  next.addEventListener("click", () => {
+    index = (index + 1) % images.length;
+    updateSlide();
+  });
+
+  prev.addEventListener("click", () => {
+    index = (index - 1 + images.length) % images.length;
+    updateSlide();
+  });
+}
