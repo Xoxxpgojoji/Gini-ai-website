a7f21510c5e51bb0ad0b8d8422105135b08aab8c
Anmol srivastava <155183109+Xoxxpgojoji@users.noreply.github.com>
2026-01-23 19:00:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const elements = document.querySelectorAll(".reveal");
+const items = document.querySelectorAll(".reveal");
 
-function revealOnScroll(){
-  elements.forEach(el => {
-    const top = el.getBoundingClientRect().top;
+function showOnScroll(){
+  items.forEach(item => {
+    const top = item.getBoundingClientRect().top;
     if(top < window.innerHeight * 0.85){
-      el.classList.add("show");
+      item.classList.add("show");
     }
   });
 }
 
-window.addEventListener("scroll", revealOnScroll);
-window.addEventListener("load", revealOnScroll);
+window.addEventListener("scroll", showOnScroll);
+window.addEventListener("load", showOnScroll);
