1b062a3968b7f1927a59287144324bba9d535589
Xoxxpgojoji <155183109+Xoxxpgojoji@users.noreply.github.com>
2026-01-23 15:49:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("scroll", () => {
-  const sections = document.querySelectorAll("section");
+  const sections = document.querySelectorAll(".section");
 
   sections.forEach(section => {
     const rect = section.getBoundingClientRect();
     const trigger = window.innerHeight / 1.2;
 
-    if (rect.top < trigger) {
+    if(rect.top < trigger){
       section.classList.add("show");
     }
   });
