026a7c05a8c701b9f3d8b39006fa9f06da8711c3
Anmol srivastava <155183109+Xoxxpgojoji@users.noreply.github.com>
2026-01-23 22:00:09 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,17 @@
-const slider = document.querySelector('[data-slider="model1"]');
+window.addEventListener("load", () => {
+  const slider = document.querySelector('[data-slider="model1"]');
+  if (!slider) return;
 
-if (slider) {
   const slides = slider.querySelector('.slides');
-  const images = slides.querySelectorAll('img');
+  const images = slider.querySelectorAll('img');
+
   let index = 0;
 
-  setInterval(() => {
+  function slideNext() {
+    const slideWidth = slider.offsetWidth;
     index = (index + 1) % images.length;
-    slides.style.transform = `translateX(-${index * images[0].clientWidth}px)`;
-  }, 1500);
-}
+    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+  }
+
+  setInterval(slideNext, 1500); // 1.5 sec auto slide
+});
